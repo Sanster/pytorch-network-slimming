c79170f5fdef41ce7e6d110e7340f379d171a82b
Sanster <cwq1913@gmail.com>
2021-09-27 08:58:50 +0800

    add mbv3 test

--- tests/helper.py
+++ tests/helper.py
@@ -3,14 +3,18 @@ import tempfile
 
 import torch
 from pns import SlimPruner
+from pns.schema_post_process import mbv3_large_schema_post_process
 from pns.tracker import gen_pruning_schema
 
 
-def check_gen_schema(model):
+def check_gen_schema(model, net: str = ""):
     x = torch.Tensor(1, 3, 224, 224)
     config = gen_pruning_schema(model, x)
 
-    with open("test.json", 'w') as f:
+    if "mobilenet_v3_large" in net and "nose" not in net:
+        mbv3_large_schema_post_process(config)
+
+    with tempfile.NamedTemporaryFile("w") as f:
         json.dump(config, f, indent=2, ensure_ascii=False)
         f.flush()
         pruner = SlimPruner(model, f.name)
